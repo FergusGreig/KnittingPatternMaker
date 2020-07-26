34a28b76672326b485f5ac3f0b497b0d4bedccd1
Fergus Greig <fergus.greig@gmail.com>
2020-07-26 17:50:28 +0100

    Added stocking stitch and swatch functions

--- knitpattern/pattfunct.cpp
+++ knitpattern/pattfunct.cpp
@@ -1,35 +1,62 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
-string rib(int nrows, int nstitch, int nknit, int npurl)
+
+
+std::string makeSwatch(std::string rightSideRow, std::string wrongSideRow, int nrows)
+{
+    int rownum = 0;
+    std::string swatch;
+    rightSideRow.append("\n");
+    wrongSideRow.append("\n");
+    while (rownum < nrows){
+        if (rownum % 2){
+            swatch.append(wrongSideRow);
+        } else {
+            swatch.append(rightSideRow);
+        }
+    }
+    return swatch;
+}
+
+std::string rib(int nrows, int nstitch, int nknit, int npurl) // alternating knits and purls to form a ribbed pattern
 {
     int i, stitches = 0;
-    string row, patch;
+    std::string RSrow, WSrow;
 
     while (stitches < nstitch)
     {
         for (i = 0; i < nknit; i++)
         {
-            row.append("k");
+            RSrow.append("k");
+            WSrow.append("p");
             stitches++;
         }
         for (i = 0; i < npurl; i++)
         {
-            row.append("p");
+            RSrow.append("p");
+            WSrow.append("k");
             stitches++;
         }
     }
-    row.append("\n");
-    for (i = 0; i < nrows; i++)
-    {
-        patch.append(row);
-    }
-    return patch;
+    return makeSwatch(RSrow,WSrow,nrows);
 }
+
+std::string stst (int nrows, int nstitch)
+{
+    int rownum = 0;
+    std::string RSrow, WSrow, swatch;
+    for (int i =0; i < nstitch; i++){
+        RSrow.append("k");
+        WSrow.append("p");
+    }
+    return makeSwatch(RSrow,WSrow, nrows);
+}
+
 
 int main()
 {
-    string patch = rib(8, 40, 2, 3);
-    std::cout<<patch;
+    std::cout << rib(8, 40, 2, 3);
+    std::cout << stst(20,35);
     return 0;
 } 
