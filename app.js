7a0372faa7dc5e8cf62af084597cafb99c5944ec
Fergus Greig <fergus.greig@gmail.com>
2020-08-09 17:05:04 +0100

    button functionality

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 // FUNCTIONS
-function makeGrid(rows,cols) {
+function makeGrid(rows, cols) {
     tableElem.innerHTML = ''; // clears the grid
-    for (var i = 0; i<rows; i++){
+    for (let i = 0; i < rows; i++) {
         row = tableElem.insertRow(i);
-        for (var j = 0; j<cols; j++){
+        for (let j = 0; j < cols; j++) {
             row.insertCell(j)
         }
     }
 }
 
 //DECLARATIONS
+var rowindex = 1;
 var width;
 var height;
 const tableElem = document.getElementById('Swatch');
+const pattElem = document.getElementById('pattern');
 const pickerElem = document.getElementById('yarn1');
+const yarnButt = document.getElementById('addYarn');
+const rowButt = document.getElementById('addRow');
+const updateButt = document.getElementById('updateSwatch')
 
 
 // EVENTS:
+// yarnButt.addEventListener("click", function(event){})
+
+updateButt.addEventListener('click', function (event) {
+    event.preventDefault();
+    makeGrid(30, 30);
+})
+rowButt.addEventListener('click', function (event) {
+    rowindex++
+    row = pattElem.insertRow()
+    index = row.insertCell();
+    index.innerHTML = rowindex;
+    for (let i = 1; i < 6; i++) {
+        row.insertCell(i)
+    }
+})
+
 // Submit button makes a new table
 // formElem.addEventListener("submit",function(event){
 //     event.preventDefault();
@@ -26,8 +47,8 @@ const pickerElem = document.getElementById('yarn1');
 // })
 
 // Clicking boxes colours them in.
-tableElem.addEventListener('click',function(event){
-    if (event.target.innerHTML == ''){ 
+pattElem.addEventListener('click', function (event) {
+    if (event.target.innerHTML == '') {
         event.target.style.backgroundColor = pickerElem.value;
     }
     /* As only the <td> elements will be blank in size this effectively 
